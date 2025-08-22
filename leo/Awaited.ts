70611b05f6e7023869111135720ec35501fce6c6
임재원 <nemo0824@naver.com>
2025-08-22 23:24:49 +0900

    refactor: Awaited

--- leo/Awaited.ts
+++ leo/Awaited.ts
@@ -3,3 +3,6 @@ type ExampleType = Promise<string>;
 type Result = MyAwaited<ExampleType>; // string
 
 type MyAwaited<T> = T extends Promise<infer R> ? R : T;
+
+// 1. Awaited에서 타입을 꺼내기위해서 Promise를 사용하고
+// 2. infer로 내부에있는값을 추론해서 꺼내오기
