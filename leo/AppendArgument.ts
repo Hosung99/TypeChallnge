cce6fed3dd71ccce36b81d22197877e06a4d9256
임재원 <nemo0824@naver.com>
2025-08-30 11:59:13 +0900

    refactor: AppendArgument

--- leo/AppendArgument.ts
+++ leo/AppendArgument.ts
@@ -8,3 +8,8 @@ type AppendArgument<T extends (...args: any) => any, K> = T extends (
 ) => infer R
   ? (...args: [...A, K]) => R
   : never;
+
+// 1. T extends 로 해당 함수가 함수인지 제약,
+// 2. K로 추가할 타입을 받기
+// 3. infer A 로 기존의 args를 추론, infer R로 리턴 타입을 추론
+// 4. 그게 맞다면? ..args에 기존 arguments 타입인 A 와 K 를 같이 쓰기
